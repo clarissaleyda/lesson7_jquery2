b4a7f67c64097462c33ddf1ae48352275645a0b4
clarissaleyda <cll14@my.fsu.edu>
2017-01-09 04:28:25 +0000

    lesson 7 final commit

---
+++ Lesson7_Assignment/Lesson7_Assignment_Files/script.js
@@ -0,0 +1,18 @@
+/* 
+Program name: Recipe Display Application
+Author: Clarissa Leyda
+Date: January 9, 2017
+Filename: script.js
+*/
+
+//displays the next element after the current target
+/*global $ */ function display(event) {
+    
+    $(event.currentTarget).next().fadeIn("slow");
+    
+}//end of display
+
+
+
+//attach event listener to h3 elements to invoke display function when clicked
+$("h3").click(display);
